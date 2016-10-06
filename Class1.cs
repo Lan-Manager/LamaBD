7e857aa7def442f6eb9c57c743a404acd7e04dc4
Tristan <kraig.tri125@gmail.com>
2016-10-06 14:17:28 -0400

    Entity framework setup

--- Class1.cs
+++ Class1.cs
@@ -8,5 +8,13 @@ namespace LamaBD
 {
     public class Class1
     {
+
+        public static bool verifyCredentials(string nomCompte, string motDePasse)
+        {
+            using (var ctx = new Connexion420())
+            {
+
+            }
+        }
     }
 }
